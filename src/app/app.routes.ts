af5d80824b4b45197e3d47c665ddcb31d756e0ee
leroy-s <asilvapusma@gmail.com>
2024-11-22 09:08:19 -0500

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RedireccionamientoComponent } from './redireccionamiento/redireccionamiento.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { GestionarCartaPresentacionComponent } from './gestionar-carta-presentacion/gestionar-carta-presentacion.component';
+import { AdministrarDocumentosComponent } from './administrar-documentos/administrar-documentos.component';
+import { MisEvaluacionesComponent } from './mis-evaluaciones/mis-evaluaciones.component';
+import { CorreoComponent } from './correo/correo.component';
+import { SidebarComponent } from './sidebar/sidebar.component'
+export const routes: Routes = [
+    {
+        path: '',
+        component: InicioComponent,
+        title: 'Componente inicio'
+    }
+    ,
+    {
+        path: 'cartappp',
+        component:  GestionarCartaPresentacionComponent,
+        title: 'Componente de cartas'
+    }
+    ,
+    {
+        path: 'documentacion',
+        component:  AdministrarDocumentosComponent,
+        title: 'Componente de documentos'
+    }
+    ,
+    {
+        path: 'evaluaciones',
+        component:  MisEvaluacionesComponent,
+        title: 'Componente de documentos'
+    }
+    ,
+    {
+        path: 'correo',
+        component:  CorreoComponent,
+        title: 'Componente de documentos'
+    }
+    ,
+    {
+        path: 'sidebar',
+        component: SidebarComponent,
+        title: 'Sidebar'
+    }
+    ,
+    {
+        path: 'redireccionamiento',
+        component:  RedireccionamientoComponent,
+        title: 'Componente de redireccionamiento'
+    }
+    ,
+    
+    {
+        path: '**',
+        redirectTo :''
+    }
+];
